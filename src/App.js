50714e1a541033b2a625add89904290e2debdd8b
Natalie Taber <nick@saleslogistix.com>
2020-01-25 14:21:11 -0800

    Organize and repurpose some code from other projects

--- src/App.js
+++ src/App.js
@@ -2,36 +2,152 @@ import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Col, Container, Jumbotron, Row} from "react-bootstrap";
+import _ from 'lodash';
+import uuid from 'uuid';
+import ReactCardFlip from 'react-card-flip';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowCircleRight, faArrowCircleLeft} from "@fortawesome/free-solid-svg-icons";
 
 /*<a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px" href="https://unsplash.com/@mockup_photos?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Mockup Photos"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style="display:inline-block;padding:2px 3px">Mockup Photos</span></a>*/
 class App extends Component {
+    /* Framework */
     constructor(props) {
         super(props);
 
         const storedData = getFromLS();
 
         this.state = {
-
+            initiative : storedData.initiative ? storedData.initiative : [
+                {name : "asdf", position : 20, guid: uuid.v4(), type: 'event'},
+                {name : "fdsa", position : 1, guid: uuid.v4(), type: 'character'},
+                {name : "Round ", position : -99, guid: uuid.v4(), type: 'roundMarker'},
+            ],
+            onDeck : storedData.onDeck ? storedData.onDeck : [],
         };
         if (!(storedData)){
-            saveToLS({
-
-            });
+            saveToLS(this.state);
         }
-    }
 
-    state = {
-        nameInput: null,
-        currentBreakpoint: "lg",
-        compactType: "vertical",
-        mounted: false,
-        showModal: false
-    };
+        let isFlipped = {};
+        this.state.initiative.forEach(tracked => {
+            isFlipped[tracked.guid] = false;
+        });
+        this.state.onDeck.forEach(tracked => {
+            isFlipped[tracked.guid] = false;
+        });
+        this.state.isFlipped = isFlipped;
+
+        this.handleFlip = this.handleFlip.bind(this);
+    }
 
     componentDidMount() {
         document.title = "InnKeeper";
     }
 
+    componentDidUpdate() {
+        saveToLS(this.state);
+    }
+
+    /* This is a reducer of sorts, which accepts a payload to be inserted into the state at the given key */
+    save = (payload, key) => {
+        this.setState((state) =>{
+            let returning = {};
+            returning[key] = payload;
+            return returning;
+        });
+    };
+
+    /* Data Manipulation */
+
+    advanceInitiative = () => {
+        let initiative = this.state.initiative;
+        let onDeck = this.state.onDeck;
+        onDeck.push(initiative.shift());
+        this.save(initiative, "initiative");
+        this.save(onDeck);
+    };
+
+    declineInitiative = () => {
+        let initiative = this.state.initiative;
+        let onDeck = this.state.onDeck;
+        initiative.unshift(onDeck.pop());
+        this.save(initiative, "initiative");
+        this.save(onDeck);
+    };
+
+    sortByInitiative = (a, b) => {
+        return a.position - b.position;
+    };
+
+    addToInitiative = (object, onDeck = true) => {
+        if (onDeck) {
+            let currentState = this.state.onDeck;
+            currentState.push(object);
+            currentState.sort(this.sortByInitiative);
+            this.save(currentState, 'onDeck');
+        }else {
+            let currentState = this.state.initiative;
+            currentState.push(object);
+            currentState.sort(this.sortByInitiative);
+            this.save(currentState, 'initiative');
+        }
+    };
+
+    handleFlip = (e, guid) => {
+        e.preventDefault();
+        let currentState = this.state.isFlipped;
+        currentState[guid] = !currentState[guid];
+        this.save(currentState, 'isFlipped');
+    };
+
+    /* DOM */
+    isFlipped = (guid) => {
+        return this.state.isFlipped[guid];
+    };
+
+    getInitiative = () => {
+        return _.map(this.state.initiative, (object, i) => {
+            return (
+                <ReactCardFlip isFlipped={this.isFlipped(object.guid)} flipDirection="horizontal" flipSpeedBackToFront={0.2} flipSpeedFrontToBack={0.2} key={object.guid}>
+                    <div className={"tracker-piece front"} onClick={(e)=>{this.handleFlip(e, object.guid)}}>
+                        <Row>
+                            <Col xs={1}>
+                                <Row>
+                                    sym
+                                </Row>
+                                <Row>
+                                    {object.position}
+                                </Row>
+                            </Col>
+                            <Col xs={10}>
+                                {object.name} {object.type}
+                            </Col>
+                        </Row>
+                    </div>
+
+                    <div className={"tracker-piece back"} onClick={(e)=>{this.handleFlip(e, object.guid)}}>
+                        <Row>
+                            Back
+                        </Row>
+                    </div>
+                </ReactCardFlip>
+            );
+        });
+    };
+
+    getCategory = (type) => {
+        return _.map(this.state.initiative.filter((el) => {
+            console.log(el);
+            return el.type === type;
+        }), (object, i) => {
+            console.log(object, type);
+            return (
+                <div>
+                    {object.name}
+                </div>
+            );
+        });
+    };
 
     render() {
         return (
@@ -46,43 +162,67 @@ class App extends Component {
                 <Jumbotron fluid>
                     <Container className={"scrollwork"}>
                         <h1>Your Friendly Local Inn(itiative) Keeper</h1>
-
-                        <p>https://www.npmjs.com/package/react-card-flip</p>
                     </Container>
                 </Jumbotron>
 
                 <Row>
                     <Col sm={3}>
-                        <div className={"text-center"}>
-                            <button className="btn-flip" data-back="Initiative" data-front="Roll"/>
-                        </div>
+                        <Row className={"text-center"}>
+                            <Col sm={4} className={'text-center'}>
+                                <button className={"fancy-btn fancy-btn-sm"}>
+                                    <i className="icon">
+                                        <FontAwesomeIcon
+                                            icon={faArrowCircleLeft}
+                                            size={"md"} transform={"up-2.5"} color={"#000"}/>
+                                    </i>
+                                </button>
+                            </Col>
+                            <Col sm={4}>
+                                <button className="btn-flip" data-back="Initiative" data-front="Roll"/>
+                            </Col>
+                            <Col sm={4} className={'text-center'} style={{'paddingRight':'0px', 'paddingLeft':'30px'}}>
+                                <button className={"fancy-btn fancy-btn-sm"}>
+                                    <i className="icon">
+                                        <FontAwesomeIcon
+                                            icon={faArrowCircleRight}
+                                            size={"md"} transform={"up-2.5"} color={"#000"}/>
+                                    </i>
+                                </button>
+                            </Col>
+                        </Row>
                         <div className={"tracker"}>
-                            K8LL M8 K8LL M8 K8LL M8 K8LL M8K8LL M8K8LL M8K8LL M8K8LL M8K8LL M8K8LL M8K8LL M8K8LL M8K8LL
-                            M8K8LL M8K8LL M8K8LL M8K8LL M8K8LL M8K8LL M8
+                            {this.getInitiative()}
                         </div>
                     </Col>
-
-                    <Col sm={3}>
-                        <p>
-                            People are actors in the world who have turns and need to take them. Track their position in the
-                            order, health, lingering effects, and otherwise!
-                        </p>
-                    </Col>
-                    <Col sm={3}>
-                        <p>
-                            Events are trackables that happen according to their absolute position in the game world; at
-                            round 5, Orcs Attack!
-                        </p>
-                    </Col>
-                    <Col sm={3}>
-                        <p>
-                            Effects constitute a subsection of trackable things that attach to actors' turns, or initiative
-                            numbers, or rounds, or nothing. They may be a lingering fire that will go out, a spell effect,
-                            or a very slow bullet flying through the air
-                        </p>
+                    <Col sm={9} style={{"paddingRight": "50px"}}>
+                        <Row className={"no-padding"}>
+                            <Col sm={4} style={{"paddingRight":"0px"}}>
+                                <div className={'text-center'}>
+                                    <button className={"fancy-btn"} data-name={"Characters"} data-new={"New Character"}/>
+                                </div>
+                                <div className={"categories"}>
+                                    {this.getCategory('character')}
+                                </div>
+                            </Col>
+                            <Col sm={4} style={{"paddingRight":"0px", "paddingLeft":"0px"}}>
+                                <div className={'text-center'}>
+                                    <button className={"fancy-btn"} data-name={"Effects"} data-new={"New Effect"}/>
+                                </div>
+                                <div className={"categories"}>
+                                    {this.getCategory('effect')}
+                                </div>
+                            </Col>
+                            <Col sm={4} style={{"paddingLeft":"0px"}}>
+                                <div className={'text-center'}>
+                                    <button className={"fancy-btn"} data-name={"Events"} data-new={"New Event"}/>
+                                </div>
+                                <div className={"categories"}>
+                                    {this.getCategory('event')}
+                                </div>
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
-
             </div>
         )
     };
@@ -92,7 +232,7 @@ function getFromLS() {
     let ls = {};
     if (global.localStorage) {
         try {
-            ls = JSON.parse(global.localStorage.getItem("PvP")) || {};
+            ls = JSON.parse(global.localStorage.getItem("InnKeepersBrew")) || {};
         } catch (e) {
             /*Ignore*/
         }
@@ -103,7 +243,7 @@ function getFromLS() {
 function saveToLS(values) {
     if (global.localStorage) {
         global.localStorage.setItem(
-            "PvP",
+            "InnKeepersBrew",
             JSON.stringify(values)
         );
     }
